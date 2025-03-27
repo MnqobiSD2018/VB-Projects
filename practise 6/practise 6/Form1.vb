0808bfecbe4a66f7c3dec334b817da02cdfae1ce
Mnqobi Samuel Dube <96314691+MnqobiSD2018@users.noreply.github.com>
2025-03-27 15:53:11 +0200

    32/41

---
+++ practise 6/practise 6/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim val1 As Single
+        Dim val2 As Single
+
+        val1 = TextBox1.Text
+        val2 = TextBox2.Text
+
+        Label4.Text = (val1 + val2) / 2
+
+        If Label4.Text >= "50" Then
+            Label4.ForeColor = Color.Green
+            MsgBox(TextBox3.Text & Chr(10) & Chr(13) & "You Did Well Keep it Up ")
+        ElseIf Label4.Text < "50" Then
+            Label4.ForeColor = Color.Red
+            MsgBox(TextBox3.Text & Chr(10) & Chr(13) & "You can do Better keep Trying as Hard as You Can")
+
+        End If
+    End Sub
+End Class
---
+++ select case and listbox/select case and listbox/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ListBox1.SelectedIndexChanged
+        'The list box item selected (0-2) is held in the SelectedIndex property
+        Select Case ListBox1.SelectedIndex
+            Case 0
+                PictureBox1.Image = System.Drawing.Image.FromFile _
+                ("c:\Users\admin\Downloads\image.png")
+            Case 1
+                PictureBox1.Image = System.Drawing.Image.FromFile _
+                ("c:\Users\admin\Downloads\big-info.png")
+            Case 2
+                PictureBox1.Image = System.Drawing.Image.FromFile _
+                ("c:\Users\admin\Downloads\install_button.png")
+            Case 3
+                PictureBox1.Image = System.Drawing.Image.FromFile _
+               ("c:\Users\admin\Downloads\image.png")
+
+        End Select
+    End Sub
+End Class
