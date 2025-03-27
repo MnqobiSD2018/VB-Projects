6f01f4fea47643fd4764a4145dc555f6a12f10c5
Mnqobi Samuel Dube <96314691+MnqobiSD2018@users.noreply.github.com>
2025-03-27 15:54:42 +0200

    33/41

---
+++ Text to Speech/Text to Speech/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim Sapi As Object
+        Sapi = CreateObject("Sapi.spvoice")
+        Sapi.speak(TextBox1.Text)
+
+    End Sub
+End Class
