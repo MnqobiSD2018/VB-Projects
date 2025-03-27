d4667ca289a44df37ee5c8da9c9dea6e555f2c20
Mnqobi Samuel Dube <96314691+MnqobiSD2018@users.noreply.github.com>
2025-03-27 16:04:46 +0200

    34/41 and 35/41

---
+++ Try CATCH/Try CATCH/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Dim Retries As Short = 0
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Try
+            PictureBox1.Image = _
+            System.Drawing.Bitmap.FromFile("d:\fileopen.bmp")
+        Catch
+            Retries += 1
+            If Retries <= 2 Then
+                MsgBox("Please insert the disc in drive D!")
+            Else
+                MsgBox("File Load feature disabled")
+                Button1.Enabled = False
+            End If
+        End Try
+
+
+    End Sub
+End Class
---
+++ Try Catch Directory test/Try Catch Directory test/Form1.vb
@@ -0,0 +1,23 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Dim retries As Integer = 1
+
+
+        Try
+
+            PictureBox1.Image = System.Drawing.Bitmap.FromFile("F:\1.bmp")
+
+        Catch ex As FileNotFoundException
+            MsgBox("Please Check If the drive is Inserted ")
+        Catch ex As OutOfMemoryException
+            MsgBox("Check the File Format")
+        Catch ex As DirectoryNotFoundException
+            MsgBox("There was a Problem Openning the File")
+
+        End Try
+    End Sub
+End Class
