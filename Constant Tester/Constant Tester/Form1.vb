5211b40a6a10a2c9acf9b4635992981fbb98a88c
Mnqobi Samuel Dube <96314691+MnqobiSD2018@users.noreply.github.com>
2025-03-27 15:25:54 +0200

    4/41

---
+++ Constant Tester/Constant Tester/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Const Pi As Double = 3.14159265
+        Label1.Text = Pi
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        End
+
+    End Sub
+End Class
---
+++ birthday app/birthday app/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        MsgBox("Your birth date was " & DateTimePicker1.Text)
+        MsgBox("Day of the year: " & _
+        DateTimePicker1.Value.DayOfYear.ToString())
+    End Sub
+End Class
